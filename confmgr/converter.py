4083c414870f04af6b96e23249e645ea3f44e9f2
Raphaël Barrois <raphael.barrois@polytechnique.org>
2012-06-20 22:53:45 +0200

    Improve Backporter/Generator/Differ.
    Signed-off-by: Raphaël Barrois <raphael.barrois@polytechnique.org>

--- confmgr/converter.py
+++ confmgr/converter.py
@@ -2,19 +2,134 @@
 # Copyright (c) 2010-2012 Raphaël Barrois
 
 
-class Converter(object):
+class FileProcessor(object):
+    """Handles 'standard' processing of a file.
+
+    Attributes:
+        src (str list): lines of the file to process
+        root (str): path to the root of the repository
+    """
     def __init__(self, src, root):
         self.src = src
         self.root = root
 
     def forward(self, categories):
-        generator = Generator(self.src)
+        """Process the source file with an active list of categories."""
+        generator = Generator(self.src, categories, self.root)
         for line in generator:
-            if line.output:
+            if line.output is not None:
                 yield line.output
 
     def backward(self, categories, modified):
-        pass
+        """Revert a file.
+
+        Args:
+            categories (str iterable): active categories
+            modified (str list): lines of the modified file
+
+        Yields:
+            str: updated lines for the original file
+        """
+        original_output = list(self.forward)
+        diff = Differ(original_output, modified)
+        generator = Generator(self.src, categories, self.root)
+        backporter = Backporter(diff, generator)
+
+        for line in backporter:
+            yield line
+
+
+class Differ(object):
+    """Computes differences between two files (as string lists).
+
+    Based on difflib.SequenceMatcher, but yields atomic operations.
+
+    Attributes:
+        original (str list): lines of the original file
+        modified (str list): lines of the modified file
+    """
+    def __init__(self, original, modified):
+        self.original = original
+        self.modified = modified
+
+    def __iter__(self):
+        """Yield atomic diff lines.
+
+        Yields:
+            (operation, new_line) tuples.
+        """
+        matcher = difflib.SequenceMatcher(a=original_output, b=modified)
+        opcodes = matcher.get_opcodes()
+        for opcode, original_i, original_j, modified_i, modified_j in opcodes:
+            if opcode == 'equal':
+                for original_lineno in range(original_i, original_j):
+                    yield (opcode, self.original[original_lineno])
+            elif opcode == 'insert':
+                for modified_lineno in range(modified_i, modified_j):
+                    yield (opcode, self.modified[modified_lineno])
+            elif opcode == 'delete':
+                for original_lineno in range(original_i, original_j):
+                    yield (opcode, self.original[original_lineno])
+            elif opcode == 'replace':
+                common = min(original_j - original_i, modified_j - modified_i)
+                for modified_lineno in range(modified_i, modified_i + common):
+                    yield (opcode, self.modified[modified_lineno])
+                for modified_lineno in range(modified_i + common, modified_j):
+                    yield ('insert', self.modified[modified_lineno])
+                for original_lineno in range(original_i + common, original_j):
+                    yield ('delete', self.original[original_lineno])
+
+
+class Backporter(object):
+    """Handles the backporting of a diff to an original file.
+
+    Attributes:
+        diff ((operation, new_line) iterable): the lines of the diff
+        source (Line iterable): the lines of the source
+    """
+
+    def __init__(self, diff, source):
+        self.diff = diff
+        self.source = source
+
+    def reverse(self, output):
+        """Convert back an output line into its original version."""
+        line = Line(output, None)
+        line.fill_original()
+        return line.original
+
+    def __iter__(self):
+        """Yield lines from the initial file."""
+        diff = iter(self.diff)
+
+        for line in self.source:
+            # Loop through the generated lines
+
+            if line.output is None:
+                # Masked line (comment, command)
+                # Always include
+                yield line.original
+
+            else:
+                action, output = diff.next()
+                while action == 'insert':
+                    # Inserting lines
+                    # Always include, without forwarding the source
+                    yield self.reverse(output)
+                    action, output = diff.next()
+
+                if action == 'delete':
+                    # Deleting one line, advance the source
+                    continue
+                else:
+                    # Replacing / equal, write the current line
+                    yield self.reverse(output)
+
+        # Handle additional lines from the diff
+        # Should only be 'insert' lines.
+        for action, output in diff:
+            assert action == 'insert'
+            yield self.reverse(output)
 
 
 class Line(object):
@@ -70,17 +185,17 @@ class Generator(object):
 
             # Some comment
             if self.comment_re.match(line):
-                yield Line('', line, False)
+                yield Line(None, line)
 
             # An escaped line
             elif self.escaped_re.match(line):
-                yield Line(line[:2] + line[3:], line, False)
+                yield Line(line[:2] + line[3:], line)
 
             # A command line (dispatch to handle_command)
             elif self.command_re.match(line):
                 command, args = self.command_re.match(line).groups()
                 self.handle_command(command, args)
-                yield Line('', line)
+                yield Line(None, line)
 
             # If displaying the line, replace placeholders.
             elif self.in_published_block:
@@ -92,7 +207,7 @@ class Generator(object):
 
             # Not displaying the line
             else:
-                yield Line('', line)
+                yield Line(None, line)
 
     def invalid(self, message, *args):
         """Generate a contextualized error message."""
