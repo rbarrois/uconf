60637d121cc782c10e5a6b48a074bf27623c0c20
Raphaël Barrois <raphael.barrois@polytechnique.org>
2012-09-02 01:19:47 +0200

    Improve config.py
    Signed-off-by: Raphaël Barrois <raphael.barrois@polytechnique.org>

--- confmgr/config.py
+++ confmgr/config.py
@@ -9,12 +9,19 @@ import re
 import subprocess
 
 # Local imports
-import log
-import misc
+from . import parser
 
 
 class Configuration(object):
-    """Holds all configuration."""
+    """Holds all configuration.
+
+    Attributes:
+        categories (str set): all categories
+        files (str list): all managed files
+        repo_root (str): absolute path of the repository root
+        install_root (str): absolute path where files should be installed
+        rule_lexer (parser.RuleLexer): lexer to use for rule parsing
+    """
 
     def __init__(self, *args, **kwargs):
         self.categories = frozenset()
@@ -37,15 +44,3 @@ class Configuration(object):
             rule = self.rule_lexer.get_rule(rule_text)
             if rule.test(self.categories):
                 self.files.append(filename)
-
-
-
-
-
-
-
-
-
-
-
-
