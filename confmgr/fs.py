b32a6b809e9440cb1f96d2c7eb91f5159f151e6f
Raphaël Barrois <raphael.barrois@polytechnique.org>
2012-09-02 02:02:25 +0200

    Add more commands to the fs module.
    Signed-off-by: Raphaël Barrois <raphael.barrois@polytechnique.org>

--- confmgr/fs.py
+++ confmgr/fs.py
@@ -5,6 +5,7 @@
 
 import io
 import os
+import stat
 
 class BaseFileSystem(object):
     """Abstraction layer around the file system.
@@ -15,8 +16,82 @@ class BaseFileSystem(object):
         self.root = root
         self.encoding = encoding
 
-    def open(self, filename, mode):
+    # Extension points
+    def _access(self, filename, mode):
         raise NotImplementedError()
+
+    def _open(self, filename, mode):
+        raise NotImplementedError()
+
+    def _stat(self, filename):
+        raise NotImplementedError()
+
+    def _chmod(self, filename, mode):
+        raise NotImplementedError()
+
+    def _chown(self, filename, uid, gid):
+        raise NotImplementedError()
+
+    def _symlink(self, link_name, target):
+        raise NotImplementedError()
+
+    # Base commands
+    def normalize_path(self, path):
+        return os.path.normpath(os.path.join(self.root, path))
+
+    def access(self, path, read=True, write=False):
+        mode = os.F_OK
+        if read:
+            mode |= os.R_OK
+        if write:
+            mode |= os.W_OK
+        return self._access(self.normalize_path(path), mode)
+
+    def open(self, filename, mode):
+        return self._open(self.normalize_path(filename), mode)
+
+    def stat(self, filename):
+        return self._stat(self.normalize_path(filename))
+
+    def chmod(self, filename, mode):
+        settable_mode = stat.S_IMODE(mode)
+        return self._chmod(self.normalize_path(filename), settable_mode)
+
+    def chown(self, filename, uid, gid):
+        return self._chown(self.normalize_path(filename), uid, gid)
+
+    def symlink(self, link_name, target):
+        return self._symlink(
+            self.normalize_path(link_name),
+            self.normalize_path(target),
+        )
+
+    # Complex commands
+    def create_symlink(self, link_name, target, relative=False, force=False):
+        if self.access(link_name):
+            file_stat = self.stat(link_name)
+            if not stat.S_ISLNK(file_stat.st_mode) and not force:
+                raise IOError(
+                    "File at %s exists and is not a symlink." % link_name)
+            elif stat.S_ISDIR(file_stat.st_mode):
+                raise IOError("%s exists and is a directory.")
+            else:
+                self.remove(link_name)
+
+        # TODO: Handle relative=True
+        self.symlink(link_name, target)
+
+    def copy(self, source, destination, copy_mode=True, copy_user=False):
+        with self.open(source, 'rb') as src:
+            with self.open(destination, 'wb') as dst:
+                dst.write(src.read())
+
+        if copy_mode or copy_user:
+            stat = self._stat(source)
+            if copy_mode:
+                self.chmod(destination, stat.st_mode)
+            if copy_user:
+                self.chown(destination, stat.st_uid, stat.st_gid)
 
     def read_one_line(self, filename):
         """Read one (stripped) line from a file.
@@ -48,20 +123,52 @@ class BaseFileSystem(object):
 
 class FileSystem(BaseFileSystem):
     """Actual filesystem layer."""
-    def open(self, filename, mode, encoding=None):
+    def _access(self, filename, mode):
+        return os.access(filename, mode)
+
+    def _open(self, filename, mode, encoding=None):
         encoding = encoding or self.encoding
-        full_path = os.path.join(self.root, filename)
-        return io.open(full_path, mode, encoding=encoding)
+        return io.open(filename, mode, encoding=encoding)
+
+    def _stat(self, filename):
+        return os.stat(filename)
+
+    def _chmod(self, filename, mode):
+        return os.chmod(filename, mode)
+
+    def _chown(self, filename, uid, gid):
+        return os.chown(filename, uid, gid)
+
+    def _symlink(self, link_name, target):
+        return os.symlink(target, link_name)
 
 
 class ReadOnlyFS(BaseFileSystem):
-    def open(self, filename, mode, encoding=None):
+
+    def _ro_forbidden(self, msg, *args):
+        raise IOError("Read-Only filesystem: " + msg % args)
+
+    def _access(self, filename, mode):
+        return os.access(filename, mode)
+
+    def _open(self, filename, mode, encoding=None):
         for m in mode:
             if m not in ('r', 'b', 't'):
-                raise IOError(
-                    "Operating on a read-only filesystem, accessing %s with "
-                    "invalid mode %s." % (filename, m)) 
+                self._ro_forbidden("Accessing %s with invalid mode %s.",
+                    filename, m)
 
         encoding = encoding or self.encoding
-        full_path = os.path.join(self.root, filename)
-        return io.open(full_path, mode, encoding=encoding)
+        return io.open(filename, mode, encoding=encoding)
+
+    def _stat(self, filename):
+        return os.stat(filename)
+
+    def _chmod(self, filename, mode):
+        self._ro_forbidden("Cannot change mode of %s.", filename)
+
+    def _chown(self, filename, uid, gid):
+        self._ro_forbidden("Cannot change uid/gid of %s to %s:%s",
+            filename, uid, gid)
+
+    def _symlink(self, link_name, target):
+        self._ro_forbidden("Cannot symlink %s to %s", link_name, target)
