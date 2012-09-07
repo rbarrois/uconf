53634a185809dbff155e68a3fc054ce539144133
Raphaël Barrois <raphael.barrois@polytechnique.org>
2012-09-08 00:09:50 +0200

    Don't reimplement pyfs.
    Signed-off-by: Raphaël Barrois <raphael.barrois@polytechnique.org>

--- confmgr/fs.py
+++ confmgr/fs.py
@@ -7,113 +7,51 @@ import io
 import os
 import stat
 
-class BaseFileSystem(object):
-    """Abstraction layer around the file system.
 
-    All files are expected to be UTF8-encoded.
-    """
-    def __init__(self, root, encoding='utf8'):
-        self.root = root
-        self.encoding = encoding
+class FileSystem(object):
+    def __init__(self, fs):
+        self.fs = fs
 
-    # Low-level directives
-    def _access(self, filename, mode):
-        raise NotImplementedError()
+    def __getattr__(self, name):
+        return getattr(self.fs, name)
 
-    def _open(self, filename, mode):
-        raise NotImplementedError()
+    def open(self, filename, mode='r', encoding=None, **kwargs):
+        base_file = self.fs.open(filename, mode, **kwargs)
+        if 't' in mode:
+            return io.open(base_file, encoding=encoding or self.encoding)
+        else:
+            return base_file
 
-    def _stat(self, filename):
-        raise NotImplementedError()
+    def read_one_line(self, filename, encoding=None):
+        """Read one (stripped) line from a file.
 
-    def _chmod(self, filename, mode):
-        raise NotImplementedError()
-
-    def _chown(self, filename, uid, gid):
-        raise NotImplementedError()
-
-    def _symlink(self, link_name, target):
-        raise NotImplementedError()
-
-    def _mkdir(self, path):
-        """Create a directory, also creating all parents if needed."""
-        raise NotImplementedError()
-
-    # Helpers
-    def normalize_path(self, path):
-        if os.path.isabs(path):
-            return path
-        return os.path.normpath(os.path.join(self.root, path))
-
-    def split_path(self, path):
-        """Convert a (normalized) path to a list of its parent.
-
-        >>> split_path('/foo/bar/baz')
-        ['/', '/foo', '/foo/bar', '/foo/bar/baz']
+        Typically used to read a password.
         """
-        if path == '/':
-            return [path]
-        parts = []
-        head = tail = path
-        while tail:
-            parts.append(head)
-            head, tail = os.path.split(path)
-            path = head
-        return reversed(parts)
+        with self.open(filename, 'rt', **kwargs) as f:
+            return f.readline().strip()
 
-    # Base commands
-    def access(self, path, read=True, write=False):
-        """Whether a file can be accessed."""
-        mode = os.F_OK
-        if read:
-            mode |= os.R_OK
-        if write:
-            mode |= os.W_OK
-        return self._access(self.normalize_path(path), mode)
+    def readlines(self, filename, encoding=None):
+        """Read all lines from a file.
 
-    def file_exists(self, path):
-        """Whether the path exists, and is a file."""
-        path = self.normalize_path(path)
-        if not self._access(path, os.F_OK):
-            return False
-        f_stat = self._stat(path)
-        return stat.S_ISREG(f_stat)
+        Yields lines of the file, stripping the terminating \n.
+        """
+        with self.open(filename, 'rt', **kwargs) as f:
+            for line in f:
+                if line and line[-1] == '\n':
+                    # Strip final \n
+                    line = line[:-1]
+                yield line
 
-    def dir_exists(self, path):
-        path = self.normalize_path(path)
-        if not self._access(path, os.F_OK):
-            return False
-        f_stat = self._stat(path)
-        return stat.S_ISDIR(f_stat)
+    def writelines(self, filename, lines, encoding=None):
+        """Write a set of lines to a file.
 
-    def mkdir(self, path):
-        path = self.normalize_path(path)
-        for part in self.split_path(path):
-            if self.access(part) and not self.dir_exists(part):
-                # Something there, but not a dir
-                raise IOError("Parent %s of %s is not a directory" %
-                    (part, path))
-            elif not self.dir_exists(part):
-                self._mkdir(part)
-
-    def open(self, filename, mode, encoding=None):
-        return self._open(self.normalize_path(filename), mode, encoding=encoding)
-
-    def stat(self, filename):
-        return self._stat(self.normalize_path(filename))
-
-    def chmod(self, filename, mode):
-        settable_mode = stat.S_IMODE(mode)
-        return self._chmod(self.normalize_path(filename), settable_mode)
-
-    def chown(self, filename, uid, gid):
-        return self._chown(self.normalize_path(filename), uid, gid)
-
-    def symlink(self, link_name, target):
-        return self._symlink(
-            self.normalize_path(link_name),
-            self.normalize_path(target),
-        )
+        A \n will be appended to lines.
+        """
+        with self.open(filename, 'wt', encoding=encoding) as f:
+            for line in lines:
+                f.write(line)
+                # Add final \n.
+                f.write('\n')
 
     # Complex commands
     def create_symlink(self, link_name, target, relative=False, force=False):
@@ -129,101 +67,3 @@ class BaseFileSystem(object):
 
         # TODO: Handle relative=True
         self.symlink(link_name, target)
-
-    def copy(self, source, destination, copy_mode=True, copy_user=False):
-        with self.open(source, 'rb') as src:
-            with self.open(destination, 'wb') as dst:
-                dst.write(src.read())
-
-        if copy_mode or copy_user:
-            stat = self._stat(source)
-            if copy_mode:
-                self.chmod(destination, stat.st_mode)
-            if copy_user:
-                self.chown(destination, stat.st_uid, stat.st_gid)
-
-    def read_one_line(self, filename):
-        """Read one (stripped) line from a file.
-
-        Typically used to read a password.
-        """
-        with self.open(filename, 'r') as f:
-            return f.readline().strip()
-
-    def readlines(self, filename):
-        """Read all lines from a file.
-
-        Yields lines of the file, stripping the terminating \n.
-        """
-        with self.open(filename, 'r') as f:
-            for line in f:
-                # Strip final \n
-                yield line[:-1]
-
-    def writelines(self, filename, lines, encoding=None):
-        """Write a set of lines to a file.
-
-        A \n will be appended to lines before writing.
-        """
-        with self.open(filename, 'w', encoding=encoding) as f:
-            for line in lines:
-                f.write(u"%s\n" % line)
-
-
-class FileSystem(BaseFileSystem):
-    """Actual filesystem layer."""
-    def _access(self, filename, mode):
-        return os.access(filename, mode)
-
-    def _open(self, filename, mode, encoding=None):
-        encoding = encoding or self.encoding
-        return io.open(filename, mode, encoding=encoding)
-
-    def _stat(self, filename):
-        return os.stat(filename)
-
-    def _chmod(self, filename, mode):
-        return os.chmod(filename, mode)
-
-    def _chown(self, filename, uid, gid):
-        return os.chown(filename, uid, gid)
-
-    def _symlink(self, link_name, target):
-        return os.symlink(target, link_name)
-
-    def _mkdir(self, path):
-        os.mkdir(path)
-
-
-class ReadOnlyFS(BaseFileSystem):
-
-    def _ro_forbidden(self, msg, *args):
-        raise IOError("Read-Only filesystem: " + msg % args)
-
-    def _access(self, filename, mode):
-        return os.access(filename, mode)
-
-    def _open(self, filename, mode, encoding=None):
-        for m in mode:
-            if m not in ('r', 'b', 't'):
-                self._ro_forbidden("Accessing %s with invalid mode %s.",
-                    filename, m)
-
-        encoding = encoding or self.encoding
-        return io.open(filename, mode, encoding=encoding)
-
-    def _stat(self, filename):
-        return os.stat(filename)
-
-    def _chmod(self, filename, mode):
-        self._ro_forbidden("Cannot change mode of %s.", filename)
-
-    def _chown(self, filename, uid, gid):
-        self._ro_forbidden("Cannot change uid/gid of %s to %s:%s",
-            filename, uid, gid)
-
-    def _symlink(self, link_name, target):
-        self._ro_forbidden("Cannot symlink %s to %s", link_name, target)
-
-    def _mkdir(self, path):
-        self._ro_forbidden("Cannot create folder %s", path)
