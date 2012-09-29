8b67d94efe0007cf6c7f9423e50d305e7ecd4c3a
Raphaël Barrois <raphael.barrois@polytechnique.org>
2012-09-29 02:19:59 +0200

    Missing __init__ file.
    Signed-off-by: Raphaël Barrois <raphael.barrois@polytechnique.org>

---
+++ confmgr/confhelpers/__init__.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+# Copyright (c) 2010-2012 Raphaël Barrois
+
+from __future__ import unicode_literals
+
+
+from .configreader import ConfigReader
+from .merged_config import Default, NormalizedDict, DictNamespace, MergedConfig
