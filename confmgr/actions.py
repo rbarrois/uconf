935fe21a177a626f81a47fdf24cc8ae597f28d69
Raphaël Barrois <raphael.barrois@polytechnique.org>
2012-10-04 00:55:26 +0200

    Enhance Actions.
    Create target folder before processing a file.
    
    Signed-off-by: Raphaël Barrois <raphael.barrois@polytechnique.org>

--- confmgr/actions.py
+++ confmgr/actions.py
@@ -3,32 +3,55 @@
 
 """Common action code."""
 
+import os.path
+
+from . import converter
+
 
 class BaseAction(object):
-    def __init__(self, source, destination, fs, **kwargs):
+    def __init__(self, source, destination, fs_config, **kwargs):
         super(BaseAction, self).__init__(**kwargs)
         self.source = source
-        self.fs = fs
+        self.destination = destination
+        self.fs_config = fs_config
+        self.fs = None
 
     def forward(self, categories):
         """Apply the action."""
+        self.fs = self.fs_config.get_forward_fs()
+        self._ensure_dir_exists(self.destination)
+        self._forward(categories)
+
+    def _forward(self, categories):
         raise NotImplementedError()
 
     def backward(self, categories):
         """Revert the action."""
+        self.fs = self.fs_config.get_backward_fs()
+        self._ensure_dir_exists(self.source)
+        self._backward(categories)
+
+    def _backward(self, categories):
         raise NotImplementedError()
+
+    def _ensure_dir_exists(self, path):
+        dirname = os.path.dirname(path)
+        self.fs.makedir(dirname, recursive=True, allow_recreate=True)
+
+    def __repr__(self):
+        return '<%s: %s>' % (self.__class__.__name__, self.source)
 
 
 class CopyAction(BaseAction):
-    def forward(self, categories):
-        self.fs.copy(self.source, self.destination)
+    def _forward(self, categories):
+        self.fs.copy(self.source, self.destination, overwrite=True)
 
-    def backward(self, categories):
-        self.fs.copy(self.destination, self.source)
+    def _backward(self, categories):
+        self.fs.copy(self.destination, self.source, overwrite=True)
 
 
 class SymLinkAction(BaseAction):
-    def forward(self, categories):
+    def _forward(self, categories):
         self.fs.create_symlink(
             link_name=self.destination,
             link_target=self.source,
@@ -39,9 +62,10 @@ class SymLinkAction(BaseAction):
 class FileContentAction(BaseAction):
     """An action based on file *contents*."""
 
-    def forward(self, categories):
+    def _forward(self, categories):
         source_lines = self.fs.readlines(self.source)
         destination_lines = self.forward_content(source_lines, categories)
+
         self.fs.writelines(self.destination, destination_lines)
 
     def forward_content(self, source_lines, categories):
@@ -56,7 +80,7 @@ class FileContentAction(BaseAction):
         """
         raise NotImplementedError()
 
-    def backward(self, categories):
+    def _backward(self, categories):
         source_lines = self.fs.readlines(self.source)
         modified_lines = self.fs.readlines(self.destination)
         updated_lines = self.backward_content(source_lines, categories, modified_lines)
