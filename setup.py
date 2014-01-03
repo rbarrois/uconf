4a3c1487c605ea0dc1b4f97b77d724ded916f9a9
Raphaël Barrois <raphael.barrois@polytechnique.org>
2014-01-03 22:37:09 +0100

    Switch to setuptools.

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@
 # Copyright (c) 2010-2013 Raphaël Barrois
 # This software is distributed under the two-clause BSD license.
 
+from __future__ import unicode_literals
+
+import codecs
 import os
 import re
 import sys
 
-from distutils.core import setup
-from distutils import cmd
+from setuptools import setup
 
 root_dir = os.path.abspath(os.path.dirname(__file__))
 
@@ -16,75 +18,13 @@ root_dir = os.path.abspath(os.path.dirname(__file__))
 def get_version(package_name):
     version_re = re.compile(r"^__version__ = [\"']([\w_.-]+)[\"']$")
     package_components = package_name.split('.')
-    path_components = package_components + ['__init__.py']
-    with open(os.path.join(root_dir, *path_components)) as f:
+    init_path = os.path.join(root_dir, *(package_components + ['__init__.py']))
+    with codecs.open(init_path, 'r', 'utf-8') as f:
         for line in f:
             match = version_re.match(line[:-1])
             if match:
                 return match.groups()[0]
     return '0.1.0'
-
-
-def read_requirements(filename):
-    dep_re = re.compile(r'^([\w_-]+)((?:>=|<=|==|!=).*)?$')
-    with open(filename, 'rt') as f:
-        lines = [l.strip() for l in f]
-    lines = [l for l in lines if l and not l.startswith('#')]
-
-    deps = []
-
-    for line in lines:
-        match = dep_re.match(line)
-        if not match:
-            raise ValueError("Invalid dependency line %r in %s" % (line, filename))
-        dep, v = match.groups()
-        if v:
-            deps.append('%s (%s)' % (dep, v))
-        else:
-            deps.append(dep)
-    return deps
-
-
-class test(cmd.Command):
-    """Run the tests for this package."""
-    command_name = 'test'
-    description = 'run the tests associated with the package'
-
-    user_options = [
-        ('test-suite=', None, "A test suite to run (defaults to 'tests')"),
-    ]
-
-    def initialize_options(self):
-        self.test_runner = None
-        self.test_suite = None
-
-    def finalize_options(self):
-        self.ensure_string('test_suite', 'tests')
-
-    def run(self):
-        """Run the test suite."""
-        try:
-            import unittest2 as unittest
-        except ImportError:
-            import unittest
-
-        if self.verbose:
-            verbosity=1
-        else:
-            verbosity=0
-
-        loader = unittest.TestLoader()
-        suite = unittest.TestSuite()
-
-        if self.test_suite == 'tests':
-            for test_module in loader.discover('.'):
-                suite.addTest(test_module)
-        else:
-            suite.addTest(loader.loadTestsFromName(self.test_suite))
-
-        result = unittest.TextTestRunner(verbosity=verbosity).run(suite)
-        if not result.wasSuccessful():
-            sys.exit(1)
 
 
 PACKAGE = 'uconf'
@@ -93,27 +33,33 @@ PACKAGE = 'uconf'
 setup(
     name=PACKAGE,
     version=get_version(PACKAGE),
+    description="UConf, a smart tool for managing config files",
+    long_description=''.join(codecs.open('README.rst', 'r', 'utf-8').readlines()),
     author="Raphaël Barrois",
-    author_email="raphael.barrois+@polytechnique.org",
-    description='UConf, a smart tool for managing config files',
+    author_email="raphael.barrois+%s@polytechnique.org" % PACKAGE,
     license="BSD",
     keywords=['configuration', 'management', 'uconf', 'confmgr', 'config'],
-    url="http://uconf.xelnor.net/",
+    url="https://github.com/rbarrois/%s/" % PACKAGE,
+    download_url="https://pypi.python.org/pypi/%s/" % PACKAGE,
     packages=[PACKAGE],
+    platforms=["OS Independent"],
     scripts=['bin/uconf'],
-    requires=read_requirements('requirements.txt'),
+    install_requires=codecs.open('requirements.txt', 'r', 'utf-8').readlines(),
+    setup_requires=[
+        'setuptools>=0.8',
+    ],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Console",
         "Intended Audience :: System Administrators",
         "License :: OSI Approved :: BSD License",
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 2 :: Only",
         "Topic :: System :: Installation/Setup",
         "Topic :: System :: Systems Administration",
     ],
-    cmdclass={'test': test},
+    test_suite='tests',
 )
